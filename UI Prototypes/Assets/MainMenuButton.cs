85ee6d601351ca1a672f6974cf5ac506f887e552
Cody <rp17000@gmail.com>
2017-03-09 11:42:55 -0700

    main menu 0.1

---
+++ UI Prototypes/Assets/MainMenuButton.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour 
+{
+	Vector3 normalPosition;
+	string currentButton;
+	float clickAmount;
+	public float distance;
+
+	void Start () 
+	{
+		normalPosition = transform.position;
+		SelectRing.passClick += Clicked;
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		transform.position = Vector3.MoveTowards(normalPosition, Camera.main.transform.position, distance);
+		currentButton = gameObject.name;
+		print (currentButton);
+	}
+
+	void OnTriggerExit()
+	{
+		transform.position = normalPosition;
+		currentButton = "";
+	}
+
+	void Clicked(float _amount)
+	{
+		switch(currentButton)
+		{
+		case "Play":
+			int scene = SceneManager.GetActiveScene().buildIndex;
+			SceneManager.LoadScene(scene + 1, LoadSceneMode.Single);
+			break;
+		case "PlayerProfile":
+			
+			break;
+		case "Options":
+			
+			break;
+		case "Quit":
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#endif
+			Application.Quit();
+			break;
+		case "":
+			break;
+		default:
+			break;
+		}
+		print("click");
+		print (_amount);
+	}
+}
---
+++ UI Prototypes/Assets/SelectRing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SelectRing : MonoBehaviour 
+{
+	Image image;
+
+	public float fillAdd;
+	public float fillSubtract;
+
+	public static Action<float> passClick;
+
+	void Start()
+	{
+		image = GetComponent<Image> ();
+		image.fillAmount = 0;
+	}
+
+	void Update () 
+	{
+		if (Input.GetButton ("Fire1"))
+			StartCoroutine (Fill ());
+		else if (image.fillAmount > 0)
+			StartCoroutine (Unfill ());
+	}
+
+	IEnumerator Fill()
+	{
+		yield return new WaitForEndOfFrame ();
+		image.fillAmount += fillAdd;	
+
+		if (image.fillAmount == 1)
+		{
+			if (passClick != null)
+				passClick (image.fillAmount);
+			image.fillAmount = 0;
+		}
+	}
+
+	IEnumerator Unfill()
+	{
+		yield return new WaitForEndOfFrame ();
+		image.fillAmount -= fillSubtract;
+		if (image.fillAmount < 0)
+			image.fillAmount = 0;
+	}
+}
