fabfa60491c6184a4cff5da4b93d719b17bb8927
Unknown <rp17000@gmail.com>
2017-04-11 16:35:35 -0600

    Main Menu 1.1

---
+++ Main Menu 2/Assets/Scripts/MainMenuButtonConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "MainMenuButtonsPrefab")]
+public class MainMenuButtonConfig : ScriptableObject 
+{
+	public GameObject mainButtons;
+	public GameObject optionsPanel;
+	public GameObject warning;
+
+	public GameObject player;
+}
